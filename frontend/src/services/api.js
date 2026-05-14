a71fb6d6c9fe48adeb6e0fb742f999439d1c0ddf
yogeshpagar04 <yogeshpagar1661@gmail.com>
2026-05-14 21:49:02 +0530

    backend-uri setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: `${import.meta.env.VITE_APP_API_URI}/tasks`,
 });
 
 export default API;
